72a3bacffff48843df7c3be1761cbb9d8328734f
doomsdayrs <doomsdayrs@gmail.com>
2023-06-16 22:08:21 -0400

    Update kotlin-lib to v1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
-	implementation("com.gitlab.shosetsuorg:kotlin-lib:v1.2.0")
+	implementation("com.gitlab.shosetsuorg:kotlin-lib:v1.3.0")
 	implementation(kotlin("stdlib"))
 	implementation(kotlin("stdlib-jdk8"))
 	implementation("org.jsoup:jsoup:1.15.3")
